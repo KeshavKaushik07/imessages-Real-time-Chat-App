a04d356d2d05618e55406907b8f38c1d9c5d2adf
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-06-22 18:15:49 +0530

    feat: add new wallpapers, sound effects, and HeroUI theme presets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>My App</h1>
+        <h1 className='bg-amber-400 flex justify-center items-center text-3xl'>My App</h1>
 
         <header>
           <Show when="signed-out">
@@ -17,6 +18,9 @@ function App() {
             <UserButton />
           </Show>
         </header>
+        <Button>
+          My BTN
+        </Button>
 
       </div>
     </>
